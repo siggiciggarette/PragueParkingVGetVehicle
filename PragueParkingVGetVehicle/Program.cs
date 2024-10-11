838ef34ca029744c697f684d2a44e592ee74795f
sigri <sigri@SAJGUD>
2024-10-11 15:37:33 +0200

    Add project files.

---
+++ PragueParkingVGetVehicle/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Get regNr input- får vara max 10 tecken
+string[] parkingSpaces = new string[100];
+
+int choice = 0;
+
+Console.WriteLine("Your vehicles registration");
+string regNumber = Console.ReadLine();
+
+if(regNumber.Length < 11|| regNumber == null || regNumber == " ")
+{
+    Console.WriteLine("Invalid registration");
+}
+
+
+Console.WriteLine("");
+//hitta input i parkingSpaces
+//ta bort det från parkingSpaces
+
+
+
+
+
